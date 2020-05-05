9ef2a4471e2b5d24f9b5439518eb7b6976664fe3
Blessing <bleble.erin@gmail.com~>
2020-05-05 14:49:38 +0100

    Updates

---
+++ precache-manifest.e62f82486fc4b87a17a606434b1a0e0c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eee58435dd4e0b2cb5c0d2da13e3261c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc176dd556e5f00e740e",
+    "url": "/static/css/2.ee241de9.chunk.css"
+  },
+  {
+    "revision": "a283f8f548a2e5b24986",
+    "url": "/static/css/main.aa583829.chunk.css"
+  },
+  {
+    "revision": "bc176dd556e5f00e740e",
+    "url": "/static/js/2.b17b96ef.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.b17b96ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a283f8f548a2e5b24986",
+    "url": "/static/js/main.dbd66372.chunk.js"
+  },
+  {
+    "revision": "24b6ffb2176e1ca673e0",
+    "url": "/static/js/runtime-main.d8ada161.js"
+  }
+]);
